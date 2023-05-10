3fd48a1af1f54f162cfe7b5fa8afa52d492c3829
NOSTVA <nostva9000@gmail.com>
2023-05-10 19:21:58 +0300

    as

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,19 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-  },
-});
-
-ReactDOM.render(
-  <ChakraProvider theme={theme}>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ChakraProvider>
     <App />
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
